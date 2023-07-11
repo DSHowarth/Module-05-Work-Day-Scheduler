9373202fc2651e8479e254f71763fbb1fbe062c3
Dash <dkshowarth@gmail.com>
2023-07-10 20:27:28 -0700

    add function for retrieving stored events on page load

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,7 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
+  // Updates the color code of all timeslots when page loads, based on local time
   $(".time-block").each(function(){
     //Take the number off the id tag, and store the current hour of the day. Convert both to numbers
     //to prevent 9 evaluating as greater than (hour) 17. No, I'm not 100% sure why that works.
@@ -30,18 +31,15 @@ $(function () {
   });
 
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+
+  $(".description").each(function(){
+    $(this).text(localStorage.getItem(this.attr("id")));
+
+  })
 
   //Function that adds current date & day of the week to the page on load
   $("#currentDay").text(dayjs().format("dddd, MMM DD YYYY"));
